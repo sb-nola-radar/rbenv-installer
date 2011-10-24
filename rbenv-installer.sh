996c71f42d624426566b11b195b8b59a1a700c3c
Michael Linderman <michael.d.linderman@gmail.com>
2011-10-24 16:28:57 -0400

    Switch to https to mitigate timeout issues

--- rbenv-installer.sh
+++ rbenv-installer.sh
@@ -17,12 +17,12 @@ fi
 
 # Install rbenv:
 if [ ! -f $RBENV_ROOT/bin/rbenv ] ; then
-  git clone git://github.com/sstephenson/rbenv.git $RBENV_ROOT
+  git clone https://github.com/sstephenson/rbenv.git $RBENV_ROOT
 fi
 
 # Install rbenv plugins:
 if [ ! -d $RBENV_ROOT/plugins/rbenv-vars ] ; then
-  git clone git://github.com/sstephenson/rbenv-vars.git $RBENV_ROOT/plugins/rbenv-vars
+  git clone https://github.com/sstephenson/rbenv-vars.git $RBENV_ROOT/plugins/rbenv-vars
 fi
 
 # Add rbenv to the path:
