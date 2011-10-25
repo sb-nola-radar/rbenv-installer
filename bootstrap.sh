975e29635e78f6665c7221d69acdf289f9189b84
Michael Linderman <michael.d.linderman@gmail.com>
2011-10-25 11:18:46 -0400

    Remove apt upgrade from boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,11 @@
 
 # Update, upgrade and install development tools:
 sudo apt-get update
-sudo apt-get -y upgrade
 sudo apt-get -y install build-essential
-sudo apt-get -y install git-core curl 
+sudo apt-get -y install git-core
 
 # Extras for Chef, RubyGems and Rails:
 sudo apt-get -y install zlib1g-dev libssl-dev wget ssl-cert
 
-# If you are on Ubuntu 10.04 LTS
+# For Ubuntu 10.04 LTS
 sudo apt-get -y install libreadline5-dev
-
-# If you are on Ubuntu 11.10:
-# sudo apt-get -y install libreadline-gplv2-dev
